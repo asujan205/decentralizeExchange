9d29a7934a13924333e61d918ab3c725cd56109d
asujan205 <asujan205@gmail.com>
2023-05-27 17:51:03 +0545

    smart contract writing

---
+++ contracts/Exchange.sol
@@ -0,0 +1,17 @@
+ // SPDX-License-Identifier: MIT
+  pragma solidity ^0.8.4;
+
+  import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+  contract Exchange is ERC20 {
+
+    address public  owner;
+    constructor ( address _owner) ERC20("Exchange", "EXC") {
+
+        require(_owner != address(0), "ERC20: mint to the zero address");
+      owner = _owner;
+    }
+
+
+
+  }
