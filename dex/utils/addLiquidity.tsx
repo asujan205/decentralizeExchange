1f02bcd1f8afa65365390760d2d18762dbea18d4
asujan205 <asujan205@gmail.com>
2023-06-03 12:50:15 +0545

    addLiquidity

--- dex/utils/addLiquidity.tsx
+++ dex/utils/addLiquidity.tsx
@@ -3,13 +3,25 @@ import { parseEther } from "ethers/lib/utils";
 
 import { nftAbi, nftAddress, tokenAbi, tokenAdd } from "../constant/abis";
 
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 import { useWeb3React } from "@web3-react/core";
+import { use } from "chai";
 
-const addLiquidity = () => {
-  const { provider, account } = useWeb3React();
-  const [ethBalance, setEthBalance] = useState<BigNumberish>(0);
-  const [tokenBalance, setTokenBalance] = useState<BigNumberish>(0);
-  const signer = provider?.getSigner();
+const addLiquidity = async (
+  ethBalance: BigNumberish,
+  tokenBalance: BigNumberish
+) => {
+  const { account, library } = useWeb3React();
+
+  const signer = useMemo(() => library?.getSigner(), [library]);
+
+  const nftContract = new Contract(nftAddress, nftAbi, signer);
+  const tokenContract = new Contract(tokenAdd, tokenAbi, signer);
+  const approve = await tokenContract.approve(nftAddress, tokenBalance);
+  await approve.wait();
+
+  const addLiquidity = await nftContract.addLiquidity(tokenBalance, {
+    value: parseEther(ethBalance.toString()),
+  });
 };
