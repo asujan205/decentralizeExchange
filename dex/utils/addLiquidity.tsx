28609b618706e05b83cfee1cfdef7f2516af709e
asujan205 <asujan205@gmail.com>
2023-06-03 12:56:02 +0545

    tokenAmount

--- dex/utils/addLiquidity.tsx
+++ dex/utils/addLiquidity.tsx
@@ -8,7 +8,7 @@ import { useState, useMemo } from "react";
 import { useWeb3React } from "@web3-react/core";
 import { use } from "chai";
 
-const addLiquidity = async (
+export const addLiquidity = async (
   ethBalance: BigNumberish,
   tokenBalance: BigNumberish
 ) => {
@@ -24,4 +24,17 @@ const addLiquidity = async (
   const addLiquidity = await nftContract.addLiquidity(tokenBalance, {
     value: parseEther(ethBalance.toString()),
   });
+
+  await addLiquidity.wait();
+};
+
+export const calculateCD = async (
+  _addEther = "0",
+  etherBalanceContract: any,
+  cdTokenReserve: any
+) => {
+  const addEther = parseEther(_addEther);
+  const CDTokenAmount = addEther.mul(cdTokenReserve).div(etherBalanceContract);
+
+  return CDTokenAmount;
 };
